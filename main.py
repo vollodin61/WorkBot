223586b585dbad00404e0da6d96fd142d29c2ef5
ilia_volodin <vollodin61@gmail.com>
2026-04-05 18:55:32 +0300

    12 Попытка запуска с ubot отправкой тестового сообщения самому себе

--- main.py
+++ main.py
@@ -23,6 +23,10 @@ async def start_message():
                 message="Бот MinZKH запущен"
             )
             info_logger(f"Юзербот отправил сообщение")
+            await BotConfig.tele_ubot.send_message(
+                entity=-1005275643764,
+                message="Бот MinZKH запущен"
+            )
         except Exception as e:
             error_logger(f"Юзербот не включен!! ОШИБКА!! {repr(e)}")
 
