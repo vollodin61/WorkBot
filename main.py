64593b31d6da46b6cc614479b78d323445d51201
ilia_volodin <vollodin61@gmail.com>
2026-04-05 18:25:52 +0300

    6 Попытка запустить обычный бот на сервере

--- main.py
+++ main.py
@@ -15,16 +15,16 @@ from src.config.conf import error_logger, info_logger, BotConfig  # todo стр�
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 
 
-async def start_message():
-    async with BotConfig.tele_ubot:
-        try:
-            await BotConfig.tele_ubot.send_message(
-                entity="me",
-                message="Бот MinZKH запущен"
-            )
-            info_logger(f"Юзербот отправил сообщение")
-        except Exception as e:
-            error_logger(f"Юзербот не включен!! ОШИБКА!! {repr(e)}")
+# async def start_message():
+#     async with BotConfig.tele_ubot:
+#         try:
+#             await BotConfig.tele_ubot.send_message(
+#                 entity="me",
+#                 message="Бот MinZKH запущен"
+#             )
+#             info_logger(f"Юзербот отправил сообщение")
+#         except Exception as e:
+#             error_logger(f"Юзербот не включен!! ОШИБКА!! {repr(e)}")
 
 
 async def on_startup(bot: Bot, dp: Dispatcher = BotConfig.dp) -> None:
